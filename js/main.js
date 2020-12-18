5d2af0707f74e01fb8cb39386af2d730ebfa538e
papgergo1997 <pap.gergo.1997@gmail.com>
2020-12-18 17:25:35 +0100

    slider-project-js-img-adding-isWorking

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const slider = document.querySelector('.slider');
+const imagePlace = document.querySelector('.image');
 const counter = document.querySelector('.counter');
 const leftArrow = document.querySelector('.left');
 const rigthArrow = document.querySelector('.right');
@@ -7,28 +9,41 @@ const information = document.querySelector('.information');
 const dotContainer = document.querySelector('.dots');
 
 
-
+//Ezt át kell majd rakni egy modulba
 const imageContainer = [
     {
         index: 1,
-        image: `url('../img/Gleccsertó-víz.jpg')`,
+        image: 'src="./img/Bagolyág nélkül-v.jpg"',
         information: 'Egy gleccsertó, valahol Izlandon'
     },
     {
         index: 2,
-        image: `url('../img/alca.jpg')`,
+        image: `src="./img/Gleccsertó-víz.jpg"`,
         information: 'Egy méh mindig tudja, hogyan álcázza magát'
     },
     {
         index: 3,
-        image: `url('../img/Tó_vízjellele.jpg')`,
+        image: `src="./img/Tó_vízjellele.jpg"`,
         information: 'Víz és levél'
     },
     {
         index: 4,
-        image: `url('../img/bee-víz.jpg')`,
+        image: `src="./img/bee-víz.jpg"`,
         information: 'Méh és virág'
     }
 
 ];
+
+function sliderFunc(){
+    for (let i = 0; i < imageContainer.length; i++) {
+        let image = document.createElement('DIV')
+        image.innerHTML = `<img ${imageContainer[0].image} alt=""></img>`;
+        imagePlace.appendChild(image);
+    }
+}
+
+sliderFunc()
+
+
+
 
