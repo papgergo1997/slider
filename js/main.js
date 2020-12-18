b779f38f0ed8b23354cf7c6eeb85c1f8baecb8f0
papgergo1997 <pap.gergo.1997@gmail.com>
2020-12-18 18:06:09 +0100

    slider-project-js-working-counter-and-text-of-image

--- js/main.js
+++ js/main.js
@@ -13,12 +13,12 @@ const dotContainer = document.querySelector('.dots');
 const imageContainer = [
     {
         index: 1,
-        image: 'src="./img/Bagolyág nélkül-v.jpg"',
+        image: `src="./img/Gleccsertó-víz.jpg"`,
         information: 'Egy gleccsertó, valahol Izlandon'
     },
     {
         index: 2,
-        image: `src="./img/Gleccsertó-víz.jpg"`,
+        image: `src="./img/alca.jpg"`,
         information: 'Egy méh mindig tudja, hogyan álcázza magát'
     },
     {
@@ -35,15 +35,17 @@ const imageContainer = [
 ];
 
 function sliderFunc(){
+    let count = 0
     for (let i = 0; i < imageContainer.length; i++) {
-        let image = document.createElement('DIV')
-        image.innerHTML = `<img ${imageContainer[0].image} alt=""></img>`;
-        imagePlace.appendChild(image);
+        count++
+        imagePlace.innerHTML = `<img ${imageContainer[i].image} alt=""></img>`;
+        information.innerText = imageContainer[i].information;        
+        counter.innerText = `${imageContainer[i].index}/${count}`// Ezzel még lesz valami
     }
 }
-
-sliderFunc()
-
+sliderFunc();
+rigthArrow.addEventListener('click', sliderFunc)
+leftArrow.addEventListener('click', sliderFunc)
 
 
 
